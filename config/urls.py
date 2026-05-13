c89329e17d0a1992dd701c0370bb1eea7370e89a
ValeriaPostylyakova <valeria.postylyakova@mail.ru>
2026-05-13 13:36:12 +0300

    first commit

---
+++ config/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+	path('api/auth/', include('apps.authentication.urls')),
+    path('api/v1/', include('apps.profiles.urls')),
+    path('api/v1/', include('apps.posts.urls')),
+    path('api/v1/', include('apps.comments.urls')),
+	path('api/v1/', include('apps.tags.urls')),
+    path('api/v1/', include('apps.categories.urls')),
+]
